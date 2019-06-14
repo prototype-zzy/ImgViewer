4b8a42d0dd5b565e58b9ea138861a506becf6ca3
hybcloud <3048130170@qq.com>
2019-06-15 06:30:44 +0800

    重构

--- IV_App.cpp
+++ IV_App.cpp
@@ -9,8 +9,9 @@ wxIMPLEMENT_APP(IV_App);
  * 检查命令行参数：
  * 如果有，将命令行的每一个参数当成path传给IVFrame的构造函数，不需要验证path的合法性，IVFrame的构造函数知道怎么处理不合法的path
  * 如果没有命令行参数，直接调用无参IVFrame()，IVFrame会自己生成引导界面
+ * 
  */
-/*测试期间，为了求方便，OnInit()函数的作用暂时是初始化wxImageHandler并生成一个对话框来选定一个图片*/
+/* 测试期间，为了求方便，OnInit()函数的作用暂时是初始化wxImageHandler并生成一个对话框来选定一个图片 */
 bool IV_App::OnInit() {
     if (!wxApp::OnInit())
         return false;
